f15ee6e2cdda152ae9a0ed0b05dfaa00fa8238b4
AnaZermeno <anazermeno19@gmail.com>
2020-09-07 14:15:46 -0600

    Ejemplo

---
+++ Ejemplo/script.js
@@ -0,0 +1,11 @@
+document.getElementById('add-item').addEventListener('click', () => {
+    const itemName = document.querySelector('#items').value;
+    const itemPrice = document.querySelector('#price').value;
+  
+    const listNode = document.createElement('li');
+    const textNode = document.createTextNode(
+      `name: ${itemName} price: ${itemPrice}`
+    );
+    listNode.append(textNode);
+    document.getElementById('list-items').append(listNode);
+  });
